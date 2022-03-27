123f023025ee56dd475c259917864d630c09a641
Pratik Devle <pratik.devle98@gmail.com>
2022-03-27 10:35:51 +0530

    feat: added hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,48 @@
+import {IMAGES} from "../images/images";
+import "../styles/home.css";
+
 const Home = () => {
   return (
     <>
       <header>
-        <div class="header-container">
-          <nav class="main-nav">
+        <div className="header-container">
+          <nav className="main-nav">
             <a href="index.html">
-              <h1 class="logo">FinQuiz</h1>
+              <h1 className="logo">FinQuiz</h1>
             </a>
-            <div class="search-bar br-sm">
-              <span class="material-icons">search</span>
+            <div className="search-bar br-sm">
+              <span className="material-icons">search</span>
               <input type="text" placeholder="search" />
             </div>
-            <ul class="right-nav">
+            <ul className="right-nav">
               <li>
-                <a href="./pages/auth/login.html" class="btn btn-primary">
+                <a href="./pages/auth/login.html" className="btn btn-primary">
                   Login
                 </a>
               </li>
               <li>
-                <div class="avatar-md avatar-txt br-full">PD</div>
+                <div className="avatar-md avatar-txt br-full">PD</div>
               </li>
             </ul>
           </nav>
         </div>
       </header>
-      <section class="container">
-        <div class="hero-ctn">
-          <div class="hero-txt">
+      <section className="container">
+        <div className="hero-ctn">
+          <div className="hero-txt">
             <h1>Test your personal finance knowledge with FinQuiz!</h1>
-            <div class="hero-btn">
-              <a href="#" class="btn btn-primary cta-btn">
+            <div className="hero-btn">
+              <a href="#" className="btn btn-primary cta-btn">
                 Get Started
               </a>
             </div>
           </div>
-          <div class="hero-img">
-            <img class="img-responsive" src="" alt="hero-image" />
+          <div className="hero-img">
+            <img
+              className="img-responsive"
+              src={IMAGES.hero}
+              alt="hero-image"
+            />
           </div>
         </div>
       </section>
