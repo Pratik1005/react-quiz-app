4ef3207d6a5d688628807a5b9a24c531e50a6865
Pratik Devle <pratik.devle98@gmail.com>
2022-04-01 10:05:31 +0530

    feat: added fetch to find selected category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,18 +1,23 @@
 import {useParams, Link} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {axios} from "axios";
+import axios from "axios";
 import {NavMenu, Footer} from "../components";
 
 const Category = () => {
   const [quizList, setQuizList] = useState([]);
+  const [categoryTitle, setCategoryTitle] = useState("");
   const {quizId} = useParams();
-
+  console.log(typeof quizId);
   useEffect(() => {
     (async () => {
       try {
         const response = await axios.get("/api/quiz");
         const quizDb = response.data.quiz;
-        const selectedCategory = quizDb.find((item) => item.id === quizId);
+        console.log(quizDb);
+        const selectedCategory = quizDb.find(
+          (item) => item.id === parseInt(quizId)
+        );
+        setCategoryTitle(selectedCategory.categoryName);
         setQuizList(selectedCategory.allQuiz);
         console.log(quizList);
       } catch (err) {
@@ -24,10 +29,11 @@ const Category = () => {
     <>
       <NavMenu />
       <section>
+        <h2 className="text-center">{categoryTitle}</h2>
         <div className="quiz-card">
           <img src="" alt="" />
           <div className="quiz-card-content">
-            <h3>Title</h3>
+            <h3>{categoryTitle}</h3>
             <Link to={`/rules/${quizId}`} className="btn btn-primary">
               Play Quiz
             </Link>
