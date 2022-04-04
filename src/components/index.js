708fcbb8a80b9f023d3b4f4d2c02dfa436abd74d
Pratik Devle <pratik.devle98@gmail.com>
2022-04-04 22:51:37 +0530

    feat: added scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {Question} from "./Question";
 export {QuizCard} from "./QuizCard";
 export {FeaturedCategory} from "./FeaturedCategory";
 export {Loader} from "./Loader";
+export {ScrollToTop} from "./ScrollToTop";
